9efa308c959c98bff79af193fff0343809e44305
vikaskr-07 <vikaskum7ar@gmail.com>
2024-10-19 21:56:24 +0530

    db.js changes

--- utils/db.js
+++ utils/db.js
@@ -2,10 +2,15 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('mongodb connected successfully');
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000, // Adjust the timeout as needed
+        });
+        console.log('MongoDB connected successfully');
     } catch (error) {
-        console.log(error);
+        console.error('MongoDB connection error:', error);
     }
 }
+
 export default connectDB;
